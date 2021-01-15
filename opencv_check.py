0aadb6d2a9e3587c57c598c909f275b111b8132a
scriptbiggie <35242020+scriptbiggie@users.noreply.github.com>
2021-01-15 15:19:46 +0300

    Add files via upload

--- opencv_check.py
+++ opencv_check.py
@@ -3,3 +3,4 @@ import re
 cv_info = [re.sub('\s+', ' ', ci.strip()) for ci in cv2.getBuildInformation().strip().split('\n') 
                if len(ci) > 0 and re.search(r'(nvidia*:?)|(cuda*:)|(cudnn*:)|(ffmpeg*:?)|(gstreamer*:?)', ci.lower()) is not None]
 print(cv_info)
+print("OpenCV version: {}".format(cv2.__version__))
